c971009c1d187685ebf0e404e06fc67cf53d135a
Denver Manik <denver.manik@rubicotech.in>
2022-06-27 18:29:21 +0530

    made static structure

---
+++ script.js
@@ -0,0 +1,19 @@
+const newYears = '1 Jan 2023' ;
+
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const seconds
+
+
+
+
+    console.log(days, hours, minutes);
+}
+
+
+countdown();
+
+setInterval(countdown, 1000);
