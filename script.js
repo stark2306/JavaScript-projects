82e5efd68056e9370cab136b86910a34a9d72680
Denver Manik <denver.manik@rubicotech.in>
2022-06-28 15:18:32 +0530

    Countdown timer done

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const newYears = '1 Jan 2023' ;
+const daysE1  = document.getElementById("days");
+const hoursE1 = document.getElementById("hours");
+const minsE1 = document.getElementById("mins");
+const secondsE1 = document.getElementById("seconds");
 
+const newYears = "1 Jan 2023" ;
 
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds /60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
 
+    daysE1.innerHTML = days; 
+    hoursE1.innerHTML = formatTime(hours);
+    minsE1.innerHTML = formatTime(mins);
+    secondsE1.innerHTML = formatTime(seconds);
+}
 
-
-    console.log(days, hours, minutes);
+function formatTime(time){
+    return time;
 }
 
 
+
+
+// initial call 
 countdown();
 
 setInterval(countdown, 1000);
